29f03841f8d17775d855448edfe36d101f798898
Mohit13333 <mohitkumar143680@gmail.com>
2024-11-03 19:35:26 +0530

    push

--- backend/Controller/contact.js
+++ backend/Controller/contact.js
@@ -4,11 +4,11 @@ const contactForm = async (req, res) => {
     const response = req.body;
     await Contact.create(response);
     res.status(200)
-      .send(`<body style="background-color:pink;"><h1 style="color:blue;">Message Sent Successfuly</h1>
+      .send(`<body style="background-color:pink;"><h1 style="color:blue;">Message Sent Successfuly<span>😊</span></h1>
             <h1 style="color:green;text-align:end;margin-top:600px">Thank you for contating</h1>
             </body>`);
   } catch (error) {
-    return res.status(500).send(`<body style="background-color:gold; text-align:center;"><h1 style="color:Red;margin-top:300px;">Message Not Delivered</h1></body>`);
+    return res.status(500).send(`<body style="background-color:gold; text-align:center;"><h1 style="color:Red;margin-top:300px;">Message Not Delivered<span>😩</span></h1></body>`);
   }
 };
 export default contactForm;
